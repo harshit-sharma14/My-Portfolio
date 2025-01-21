655728ba426bd6e67c92f5a7a857e2151ce799a0
harshit-sharma14 <harshit14922@gmail.com>
2025-01-22 01:18:10 +0530

    Some more deployment changes in button

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -13,14 +13,14 @@ export const Projects = () => {
             <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1}} className='w-full lg:w-1/4'>
             <img src={photo2} width={250} height={250} className='mb-6 rounded' alt="" />
             </motion.div>
-            <div className='w-full max-w-xl lg:w-3/4'>
+            <div className='w-full h-auto max-w-full lg:w-3/4'>
             <h3 className='mb-2 font-semibold text-2xl'>Hotel Booking Website</h3>
-            <p className='mb-4 text-stone-400'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus cumque nam quia unde voluptates ipsum beatae, similique aliquam? Dolor nostrum, culpa nisi unde quibusdam voluptate suscipit impedit. Veritatis, ad perferendis!</p>
-            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium my-2 text-stone-300'>React.js</span>
-            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium my-2 text-stone-300'>Tailwind CSS</span>
-            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium my-2 text-stone-300'>MongoDB</span>
-            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium my-2 text-stone-300'>Nodejs</span>
-            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium my-2 text-stone-300'>Express</span>
+            <p className='mb-4 text-stone-400 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus cumque nam quia unde voluptates ipsum beatae, similique aliquam? Dolor nostrum, culpa nisi unde quibusdam voluptate suscipit impedit. Veritatis, ad perferendis!</p>
+            <button className='m-2 rounded bg-stone-900 p-2 text-sm font-medium w-auto  text-stone-300'>React.js</button>
+            <button className='m-2 px-2 text-center w-auto rounded bg-stone-900 p-2  text-sm font-medium   text-stone-300'>Tailwind CSS</button>
+            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium   text-stone-300'>MongoDB</span>
+            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium   text-stone-300'>Nodejs</span>
+            <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium  text-stone-300'>Express</span>
             
             </div>
         </div>
