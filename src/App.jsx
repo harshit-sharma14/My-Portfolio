2d74622e5936b66dd0a4ec795b56d93b916f2264
harshit-sharma14 <harshit14922@gmail.com>
2025-02-24 04:24:23 +0530

    Final Changes removed errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Tech } from './Tech'
 import { Projects } from './Projects'
 import { Contact } from './Contact'
 import { Card } from './Card'
-import { Deco } from './Deco'
 import { Navbar1 } from './Navbar1'
 function App() {
 
@@ -14,7 +13,7 @@ function App() {
     <div className='overflow-hidden text-stone-300 antialiased'>
       <div className='fixed inset-0 -z-10'>
         <div className='relative h-full w-full '>
-        <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>
+        <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>
         </div>
       </div>
     <div className='container  mx-auto px-8'>
